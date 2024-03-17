6825804b034689e596fa84acd2a50e01c2e941c9
barbosapamella <barbosapamella21@gmail.com>
2024-03-17 19:51:35 -0300

    first commit

---
+++ 07 - Conversão de polegadas.py
@@ -0,0 +1,21 @@
+'''
+Conversão de polegadas
+Por Lucio Nunes de Lira & Rafael Maximo Carreira Ribeiro,  Brazil
+
+Timelimit: 1
+Megan quer comprar um novíssimo smartphone e está muito empolgada com as possibilidades que uma tela de várias polegadas pode oferecer para seu entretenimento. Mas há um problema, Megan percebeu que não sabe lidar com polegadas, afinal sempre realizou cálculos usando centímetros e gostaria de se basear nessa unidade de medida para imaginar o tamanho da tela de seu futuro smartphone. Você pode ajudar Megan?
+Seu trabalho é construir um programa que receba como entrada um número real, simbolizando uma quantidade de polegadas, e exiba o equivalente em centímetros. Lembre-se que uma polegada equivale a 2,54 cm.
+Entrada
+Um número real representando uma quantidade de polegadas.
+Saída
+Um número real, com exatamente três casas decimais, representando a conversão da quantidade de polegadas da entrada na respectiva quantidade em centímetros.
+Samples Input
+10.0
+Samples Output
+25.400
+'''
+
+polegadas=float(input())
+centimetros=polegadas*2.54
+
+print (f"{centimetros:.3f}")
